29dc3344c266a76374fa9b7a58e35b2d5973793c
yusufABC <yusufabdullah20000@gmail.com>
2025-05-24 23:34:09 +0600

    added google sign in sign up

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -2,7 +2,8 @@ import React, { use } from 'react';
 import { NavLink, useNavigate } from 'react-router';
 import { AuthContext } from '../context/AuthContext';
 import Swal from 'sweetalert2';
-
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebase.init';
 const SignIn = () => {
     const { signInUser, setUser } = use(AuthContext)
     const navigate = useNavigate()
@@ -40,7 +41,21 @@ const SignIn = () => {
                 console.log(error.message)
             })
 
+            // google sign in 
+
     }
+    const handleGoogleSubmit = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then(result => {
+                setUser(result.user);
+                navigate('/');
+            })
+            .catch(error => {
+                console.log('Google login error:', error.message);
+            });
+    };
+
     return (
         <div>
             <div className="mx-auto my-20 w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-50 text-gray-800">
@@ -65,7 +80,7 @@ const SignIn = () => {
                     <div className="flex-1 h-px sm:w-16 bg-gray-300"></div>
                 </div>
                 <div className="flex justify-center space-x-4">
-                    <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                    <button onClick={()=>handleGoogleSubmit()} aria-label="Log in with Google" className="p-3 rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                             <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                         </svg>
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,13 +1,14 @@
 import React, { use, useState } from 'react';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
 import { AuthContext } from '../context/AuthContext';
 import Swal from 'sweetalert2';
-import { updateProfile } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase.init';
 
 const Signup = () => {
     const [errorMsg, setErrorMsg] = useState('')
-    const { createUser, } = use(AuthContext)
+    const {setUser ,createUser } = use(AuthContext)
+    const navigate = useNavigate()
     const handleSubmit = e => {
         e.preventDefault()
         const form = e.target
@@ -70,6 +71,7 @@ const Signup = () => {
                                 showConfirmButton: false,
                                 timer: 3500
                             });
+                            navigate('/')
                         }
                     })
             })
@@ -79,6 +81,26 @@ const Signup = () => {
             });
 
     }
+
+        const handleGoogleSubmit = () => {
+            const provider = new GoogleAuthProvider();
+            signInWithPopup(auth, provider)
+                .then(result => {
+                    setUser(result.user);
+                      Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Logged in with Google successfully!",
+                showConfirmButton: false,
+                timer: 3000
+            });
+                    navigate('/');
+                })
+                .catch(error => {
+                    console.log('Google login error:', error.message);
+                });
+        };
+    
 
     return (
         <div className="my-20 mx-auto w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-100 text-gray-800">
@@ -110,7 +132,7 @@ const Signup = () => {
                 <div className="flex-1 h-px sm:w-16 bg-gray-300"></div>
             </div>
             <div className="flex justify-center space-x-4">
-                <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                <button aria-label="Log in with Google" onClick={()=>handleGoogleSubmit()} className="p-3 rounded-sm">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
                         <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
                     </svg>
